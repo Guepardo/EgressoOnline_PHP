385d6b1797377ef3dba49dffa53167fd6cce381f
Guepardo <bsinet@hotmail.com>
2015-07-26 14:24:26 -0300

    Sync

--- View/CustomViews/TelaPrincipalView.php
+++ View/CustomViews/TelaPrincipalView.php
@@ -2,7 +2,7 @@
 namespace View\CustomViews; 
 
 use View\GenericView; 
-use DAO\CustomViews\DAORegiao; 
+use DAO\CustomDAOs\DAORegiao; 
 
 
 class TelaPrincipalView extends GenericView{
@@ -12,19 +12,29 @@ class TelaPrincipalView extends GenericView{
 	}
 	
 	public function principalView(){
-		$daoRegiao = DAORegiao(); 
+		$daoRegiao = new DAORegiao(); 
+		parent::getTemplateByAction('tela'); 
 
 		foreach( $daoRegiao->selectAllCountries() as $country ){
 			parent::$templator->setVariable("emprego.pais.value", $country->getId() ); 
-			parent::$templator->setVariable("emprego.pais.desc", $country->getDescricao()); 
+			parent::$templator->setVariable("emprego.pais.desc", utf8_encode($country->getDescricao())); 
 			parent::$templator->setVariable("pos.pais.value", $country->getId() ); 
-			parent::$templator->setVariable("pos.pais.desc", $country->getDescricao()); 
+			parent::$templator->setVariable("pos.pais.desc", utf8_encode($country->getDescricao())); 
 
 			parent::$templator->addBlock("emprego.pais");
 			parent::$templator->addBlock("pos.pais"); 
 		}
 
-		parent::getTemplateByAction('tela'); 
+		foreach( $daoRegiao->selectAllStages("BRASIL") as $estado ){
+			parent::$templator->setVariable("emprego.estado.value", $estado->getId() ); 
+			parent::$templator->setVariable("emprego.estado.desc", utf8_encode($estado->getDescricao())); 
+			parent::$templator->setVariable("pos.estado.value", $estado->getId() ); 
+			parent::$templator->setVariable("pos.estado.desc", utf8_encode($estado->getDescricao())); 
+
+			parent::$templator->addBlock("emprego.estado");
+			parent::$templator->addBlock("pos.estado"); 
+		}
+		
 		parent::show(); 
 	}
 }
