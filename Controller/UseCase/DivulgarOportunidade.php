7e1210c901cf172a8d7c35d8d4e8eef16d87e155
Guepardo <bsinet@hotmail.com>
2015-09-10 20:32:02 -0300

    sync

--- Controller/UseCase/DivulgarOportunidade.php
+++ Controller/UseCase/DivulgarOportunidade.php
@@ -47,7 +47,9 @@ class DivulgarOportunidade extends GenericController {
 		$opEmprego->oportunidadeId = $oportunidade->id; 
 		$opEmprego->insert(); 
 
-		die(json_encode(array('status' => true ) )); 
+		echo(json_encode(array('status' => true ) )); 
+
+		self::notificacaoEmprego($oportunidade->id); 
 	}
 
 	public function posGraduacao($arg){
@@ -90,7 +92,9 @@ class DivulgarOportunidade extends GenericController {
 		$pos->area = $arg['area']; 
 		$pos->insert(); 
 
-		die(json_encode(array('status' => true ) )); 
+		echo(json_encode(array('status' => true ) )); 
+
+		self::notificacaoPos($oportunidade->id); 
 	}
 
 	//$id do registo da notificação recem cadastrada: 
@@ -101,7 +105,7 @@ class DivulgarOportunidade extends GenericController {
  		Lumine::import("OpPosGraduacao");
 
  		Lumine::import("Usuario"); 
- 		Lumine::import("notificacao"); 
+ 		Lumine::import("Notificacao"); 
 
  		$op = new Oportunidade(); 
  		$opPos = new OpPosGraduacao(); 
@@ -125,7 +129,64 @@ class DivulgarOportunidade extends GenericController {
  		}
 
  		foreach($emailList as $a){
- 			$mail->sendEmail("Uma vaga de pós-graduação do seu interesse foi divulgada.", $a['email'],"EgressoOnline UEG - Vaga de pós-graduação", $a['nome']); 
+ 			//$mail->sendEmail("Uma vaga de pós-graduação do seu interesse foi divulgada.", $a['email'],"EgressoOnline UEG - Vaga de pós-graduação", $a['nome']); 
+ 		}
+	}
+
+	//$id do registo da notificação recem cadastrada: 
+	public function notificacaoEmprego($id){
+		$id = (int) $id['id']; 
+
+ 		Lumine::import("Oportunidade"); 
+ 		Lumine::import("OpEmprego");
+
+ 		Lumine::import("Usuario"); 
+ 		Lumine::import("Notificacao"); 
+ 		Lumine::import("NotificacaoHasAtuacaoProfissional"); 
+
+ 		$op 	   = new Oportunidade(); 
+ 		$opEmprego = new OpEmprego(); 
+
+ 		$op->join($opEmprego)->where("id = $id")->find(); 
+ 		$op->fetch(true); 
+
+
+ 		if($op->atuacaoProfissionalId == null )
+ 			die("não é um emprego"); //Não é um emprego; 
+
+ 		$usuario     = new Usuario(); 
+ 		$notificacao = new Notificacao(); 
+
+ 		$usuario->find(); 
+  		
+  		//Guardando os emails com o interesse relacionado. 
+  		$emailList = array(); 
+  		$mail 	   = new Mail(); 
+
+ 		while($usuario->fetch()){
+ 			$notificacao = new Notificacao(); 
+ 			$total = $notificacao->get($usuario->id); 
+
+ 			if($total < 0)
+ 				continue; 
+
+ 			$idNotificacao = $notificacao->id; 
+
+ 			$associativa = new NotificacaoHasAtuacaoProfissional(); 
+ 			$total = $associativa->get($idNotificacao); 
+
+ 			if($total < 0 )
+ 				continue; 
+
+ 			do{
+ 				if($associativa->atuacaoProfissionalId == $op->atuacaoProfissionalId)
+ 					array_push($emailList, array('nome' => $usuario->nome, 'email' => $usuario->email));  
+
+ 			}while($associativa->fetch()); 
+ 		}
+
+ 		foreach($emailList as $a){
+ 			//$mail->sendEmail("Uma vaga de emprego do seu interesse foi divulgada.", $a['email'],"EgressoOnline UEG - Vaga de pós-graduação", $a['nome']); 
  		}
 	}
 
